293606782b5eda7a974d08f6330e35d49e155ed7
chb <1642539004@qq.com>
2023-10-18 19:16:15 +0800

    加入创建数据库和表的各项配置。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,10 +32,14 @@ android {
 }
 
 dependencies {
-
+    implementation("org.litepal.android:core:1.4.1")
+    implementation("com.squareup.okhttp3:okhttp:3.4.1")
+    implementation("com.google.code.gson:gson:2.7")
+    implementation("com.github.bumpetech.glide:glide:3.7.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(files("libs\\litepal-2.0.0.jar"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
